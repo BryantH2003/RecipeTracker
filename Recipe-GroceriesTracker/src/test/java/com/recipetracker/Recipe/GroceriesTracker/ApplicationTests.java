c4876484c4fdc2f709db5824b8191db6600a9b1c
BryantH2003 <88125614+BryantH2003@users.noreply.github.com>
2024-10-01 17:02:53 -0500

    Initial Commit

---
+++ Recipe-GroceriesTracker/src/test/java/com/recipetracker/Recipe/GroceriesTracker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipetracker.Recipe.GroceriesTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
